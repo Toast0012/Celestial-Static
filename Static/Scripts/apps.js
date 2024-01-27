2af59e187923e812bfa04fb6234acef566e7883d
Toast <123399463+Toast0012@users.noreply.github.com>
2024-01-27 22:01:39 +0000

    Add files via upload

---
+++ Static/Scripts/apps.js
@@ -0,0 +1,57 @@
+function google() {
+  agU = Ultraviolet.codec.xor.encode('https://google.com');
+  const prefixedUrl = "/a/" + agU;
+  sessionStorage.setItem('ur', prefixedUrl);
+  location.href = "../g.html";
+}
+
+function youtube() {
+  agU = Ultraviolet.codec.xor.encode("https://youtube.com");
+  const prefixedUrl = "/a/" + agU;
+  sessionStorage.setItem('ur', prefixedUrl);
+  location.href = "../g.html"
+}
+
+
+function chess() {
+  agU = Ultraviolet.codec.xor.encode('https://chess.com');
+  const prefixedUrl = "/a/" + agU;
+  sessionStorage.setItem('ur', prefixedUrl);
+  location.href = "../g.html"
+}
+
+function discord() {
+  agU = Ultraviolet.codec.xor.encode('https://discord.com');
+  const prefixedUrl = "/a/" + agU;
+  sessionStorage.setItem('ur', prefixedUrl);
+  location.href = "../g.html";
+}
+
+function vscode() {
+  agU = Ultraviolet.codec.xor.encode('https://vscode.dev');
+  const prefixedUrl = "/a/" + agU;
+  sessionStorage.setItem('ur', prefixedUrl);
+  location.href = "../g.html";
+}
+
+function github() {
+  agU = Ultraviolet.codec.xor.encode('https://github.com');
+  const prefixedUrl = "/a/" + agU;
+  sessionStorage.setItem('ur', prefixedUrl);
+  location.href = "../g.html";
+}
+
+function crazygames() {
+  agU = Ultraviolet.codec.xor.encode('https://crazygames.com');
+  const prefixedUrl = "/a/" + agU;
+  sessionStorage.setItem('ur', prefixedUrl);
+  location.href = "../g.html";
+} 
+
+function poki() {
+  agU = Ultraviolet.codec.xor.encode('https://poki.com');
+  const prefixedUrl = "/a/" + agU;
+  sessionStorage.setItem('ur', prefixedUrl);
+  location.href = "../g.html";
+}
+
