3772dd6ceff6aa7b48480f141f641f5ed55989fb
Toast <123399463+Toast0012@users.noreply.github.com>
2024-02-02 10:19:59 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/a/',
-    bare: 'https://noleaking.iii.cl/bare/',
+    bare: 'https://mathdocs.freedns.rocks/outerspace/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
