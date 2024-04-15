2b459488453cff4aaaa49116242ad9bf446a2858
Toast <123399463+Toast0012@users.noreply.github.com>
2024-04-14 20:24:52 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,18 @@
+if (localStorage.bare == "lightspeed") {
+  var bare = "https://dang.iii.cl/bare/";
+} else if (localStorage.bare == "contentkeeper") {
+  var bare = "https://dontsnitchbro.iii.cl/bare/";
+} else if (localStorage.bare == "goguardian") {
+  var bare = "https://example.com"
+} else if (localStorage.bare == null) {
+  var bare = "https://dang.iii.cl/bare/"
+} else {
+  var bare = "https://dang.iii.cl/bare/"
+}
+
 self.__uv$config = {
     prefix: '/a/',
-    bare: 'https://celestial.iii.cl/v/',
+    bare: bare,
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
