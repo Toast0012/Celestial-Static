27bbb7d6c12abb93af49d62132c94f192aeff7d6
Toast <123399463+Toast0012@users.noreply.github.com>
2024-04-14 20:47:55 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,13 +1,16 @@
-var bare = "https://dang.iii.cl"
+var bare = "https://dang.iii.cl";
 if (localStorage.bare == "lightspeed") {
   bare = "https://dang.iii.cl/bare/";
 } else if (localStorage.bare == "contentkeeper") {
   bare = "https://dontsnitchbro.iii.cl/bare/";
 } else if (localStorage.bare == "goguardian") {
   bare = "https://example.com";
-  } else if (localStorage.bare == null) {
+} else if (localStorage.bare == null) {
   bare = "https://dang.iii.cl/bare/";
-  }
+} else {
+  bare = "https://dang.iii.cl/bare/";
+} 
+
 
 self.__uv$config = {
     prefix: '/a/',
